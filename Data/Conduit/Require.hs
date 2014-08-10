cdaed197bcdef35888634f4e7c185f42b3198b6d
bartavelle <bartavelle@gmail.com>
2014-08-10 22:31:52 +0200

    Potentially reduced the speed of the conduit, fixed the docs.

--- Data/Conduit/Require.hs
+++ Data/Conduit/Require.hs
@@ -20,20 +20,23 @@ module Data.Conduit.Require (withRequirement) where
 import Data.Conduit
 import Control.Dependency
 import qualified Data.Map.Strict as M
-import qualified Data.Set as S
 import Data.Monoid
 import Control.Monad
 import Control.Monad.Trans
 
 -- | Given a stream of values, from which an identifier and a content can
--- be extracted (for example a)
+-- be extracted, runs a list of computation that depend on these.
 --
+-- Each computation can only be run once, and its output is `yield`ed
+-- downstream.
+--
+-- When all computations have been run, the conduit finishes processing.
 withRequirement :: (Ord identifier, Eq identifier, Monad m)
                 => [Require identifier content x] -- ^ The list of dependent computations
                 -> (a -> identifier)              -- ^ Extracting the identifier
-                -> (a -> m content)               -- ^ Extracting the content
+                -> (a -> m content)               -- ^ Extracting the content, possibly with effects
                 -> Conduit a m x
-withRequirement computations getIdentifier getContent = run compmap imap getIdentifier getContent (fmap initialState compmap)
+withRequirement computations getIdentifier getContent = run compmap imap getIdentifier getContent (fmap (const mempty) compmap)
     where
         compmap = M.fromList (zip [0..] computations)
         imap = M.fromListWith (++) $ do
@@ -41,13 +44,12 @@ withRequirement computations getIdentifier getContent = run compmap imap getIden
             i <- getIdentifiers c
             return (i, [n])
 
-        -- run :: M.Map Int (S.Set identifier, M.Map identifier content) -> Conduit a m x
 run :: (Ord identifier, Eq identifier, Monad m)
     => M.Map Int (Require identifier content x)
     -> M.Map identifier [Int]
     -> (a -> identifier)
     -> (a -> m content)
-    -> M.Map Int (S.Set identifier, M.Map identifier content)
+    -> M.Map Int (M.Map identifier content)
     -> Conduit a m x
 run compmap imap getIdentifier getContent curmap = do
     mi <- await
@@ -58,20 +60,15 @@ run compmap imap getIdentifier getContent curmap = do
                 matchedComputations = M.findWithDefault [] ident imap
                 checkComputation (cmap, ccomp) cid =
                     case M.lookup cid cmap of
-                        Just (todo, cnt) -> do
+                        Just cnt -> do
                             extractedContent <- getContent i
-                            let ntodo = S.delete ident todo
-                                ncnt  = M.insert ident extractedContent cnt
-                                nmap  = M.insert cid (ntodo, ncnt) cmap
-                            return $ if S.null ntodo
-                                then case M.lookup cid compmap >>= computeRequire ncnt of
+                            let ncnt  = M.insert ident extractedContent cnt
+                                nmap  = M.insert cid ncnt cmap
+                            return $ case M.lookup cid compmap >>= computeRequire ncnt of
                                          Just v -> (M.delete cid cmap, v : ccomp)
                                          Nothing -> (nmap, ccomp)
-                                else (nmap, ccomp)
                         Nothing -> return (cmap, ccomp) -- this should not happen !
             (newmap, comps) <- lift (foldM checkComputation (curmap, []) matchedComputations)
             mapM_ yield comps
-            run compmap imap getIdentifier getContent newmap
+            unless (M.null compmap) $ run compmap imap getIdentifier getContent newmap
 
-initialState :: (Ord identifier, Eq identifier) => Require identifier content x -> (S.Set identifier, M.Map identifier content)
-initialState r = (S.fromList (getIdentifiers r), mempty)
